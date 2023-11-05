b327d3c546ae5cd096c82ac819df10bf02659962
Jona <87903362+samadhimogwai2@users.noreply.github.com>
2023-11-05 16:42:19 +0900

    login-logout

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+from enum import unique
 from flask import Flask
 from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
+# from flask_bootstrap import BOOTSTRAP_VERSION, Bootstrap
+
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
 from datetime import datetime
 import pytz
 
@@ -8,8 +15,13 @@ import pytz
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config["SECRET_KEY"] = os.urandom(24)
 # initialize the app with the extension
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 # db.Modelを継承してテーブルを定義
 class Post(db.Model):
@@ -17,17 +29,60 @@ class Post(db.Model):
     title = db.Column(db.String(50), nullable=False)
     body = db.Column(db.String(300), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+    
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(12))
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/', methods=['GET', 'POST'])
+@login_required  #ログインしているユーザのみアクセス可能
 def index():
     if request.method == 'GET':
         # 全てのデータを取得
         posts = Post.query.all()
 
         return(render_template('index.html', posts=posts))
+    
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User(username=username, password=generate_password_hash(password))
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    else:
+        return render_template('signup.html')
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect('/')
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+@login_required  #ログインしているユーザのみアクセス可能
+def logout():
+     logout_user()
+     return redirect('/login')
 
 @app.route('/create', methods=['GET', 'POST'])
+@login_required  #ログインしているユーザのみアクセス可能
 def create():
     if request.method == 'POST':
         title = request.form.get('title')
@@ -45,6 +100,7 @@ def create():
         return(render_template('create.html'))
     
 @app.route('/<int:id>/update', methods=['GET', 'POST'])
+@login_required  #ログインしているユーザのみアクセス可能
 def update(id):
     # データを取得
     post = Post.query.get(id)
@@ -61,6 +117,7 @@ def update(id):
         return redirect('/')
 
 @app.route('/<int:id>/delete', methods=['GET'])
+@login_required  #ログインしているユーザのみアクセス可能
 def delete(id):
     # データを取得
     post = Post.query.get(id)
